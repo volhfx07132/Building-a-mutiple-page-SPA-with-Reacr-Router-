bcbcc61d015b2c82c568d4d26704368437069017
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-16 16:43:14 +0700

    Adding Dynamic Routes with Params

---
+++ my-app/src/pages/ProductDetail.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  return (
+    <section>
+      <h1>Product Detail</h1>
+    </section>
+  );
+};
+
+export default ProductDetail;
--- my-app/src/pages/Products.js
+++ my-app/src/pages/Products.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Products = () => {
-    return <h1>The Products Page</h1>
-}
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+      </ul>
+    </section>
+  );
+};
 
 export default Products;
