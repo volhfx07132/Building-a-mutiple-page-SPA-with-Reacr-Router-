6d3959ea642afa5fbf51f786afb769b585194afa
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-17 15:46:01 +0700

    Redirecting The User

--- router-tutorial/src/pages/Products.js
+++ router-tutorial/src/pages/Products.js
@@ -1,6 +1,4 @@
-import React from "react";
 import { Link } from 'react-router-dom';
-
 
 const Products = () => {
   return (
@@ -8,15 +6,13 @@ const Products = () => {
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to="/products/p1">A Book</Link>
+          <Link to='/products/p1'>A Book</Link>
         </li>
-
         <li>
-          <Link to="/products/p2">A Carpet</Link>
+          <Link to='/products/p2'>A Carpet</Link>
         </li>
-
         <li>
-          <Link to="/products/p3">An Online Course</Link>
+          <Link to='/products/p3'>An Online Course</Link>
         </li>
       </ul>
     </section>
